f3878e8aa198acaf69bc06c0f62d688f451ad088
Rajeev Patil <rajeevvpatil899@gmail.com>
2022-10-22 15:55:24 -0700

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { JourneyComponent } from './journey/journey.component';
+import { ResumeComponent } from './resume/resume.component';
+import { SkillsComponent } from './skills/skills.component';
+import { LinksComponent } from './links/links.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    JourneyComponent,
+    ResumeComponent,
+    SkillsComponent,
+    LinksComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
